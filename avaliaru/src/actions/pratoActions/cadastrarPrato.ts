6fa95421d8ab0bc4a7387c2800c3b31754434a08
LuisGFNunes <ferreiranunesluisgustavo447@gmail.com>
2026-06-07 11:49:31 -0300

    feat: cadastrar prato

--- avaliaru/src/actions/pratoActions/cadastrarPrato.ts
+++ avaliaru/src/actions/pratoActions/cadastrarPrato.ts
@@ -1,17 +1,41 @@
 "use server";
 
+import { auth } from "@/auth";
 import { db } from "@/lib/db/db";
 import { prato } from "@/lib/db/schema";
+import { revalidatePath } from "next/cache";
 
 export async function cadastrarPrato(formData: FormData) {
+    const session = await auth();
+    
+    if (!session || !session.user) {
+        throw new Error("Não autorizado.");
+    }
+
+    const perfil = (session.user as any).role; 
+
+    const temAcesso = perfil === "gestorru" || perfil === "adm";
+
+    if (!temAcesso) {
+        throw new Error("Acesso negado: você não tem permissão.");
+    }
+
     const codigo = formData.get("codigo") as string;
     const nome = formData.get("nome") as string;
 
-    const result = db.insert(prato).values({ idPrato: codigo, nome }).run()
+    try {
 
-    if (!result) {
-        throw new Error("Erro ao cadastrar o prato");
+        await db.insert(prato).values({
+            idPrato: codigo,
+            nome: nome
+        });
+
+        revalidatePath("/gestao/pratos");
+
+        return { success: true};
+    } catch(error) {
+        console.error(error);
+        throw new Error("Erro ao salvar no banco de dados.");
     }
 
-    return result;
 }
