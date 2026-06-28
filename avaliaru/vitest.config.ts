f58ed5577085bbf6e3eee3693b251a8b146cb4ac
mateusaa <mateus.a.araujo2006@gmail.com>
2026-06-28 19:18:34 -0300

    fix: fix Ci test integration

--- avaliaru/vitest.config.ts
+++ avaliaru/vitest.config.ts
@@ -1,23 +1,18 @@
 import { fileURLToPath } from "node:url";
-import { loadEnv } from "vite";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
-
-	return {
-		resolve: {
-			alias: {
-				"@": fileURLToPath(new URL("./src", import.meta.url)),
-			},
+export default defineConfig({
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
-		test: {
-			environment: "node",
-			include: ["**/*.test.ts", "**/*.tests.ts", "**/*.integration.test.ts"],
-			env,
-			globalSetup: "./src/test/global-setup.ts",
-			setupFiles: ["./src/test/setup.ts"],
-			fileParallelism: false,
-		},
-	};
+	},
+	test: {
+		environment: "jsdom",
+		globalSetup: "./src/test/global-setup.ts",
+		setupFiles: ["./src/test/setup.ts"],
+		css: true,
+		exclude: [...configDefaults.exclude],
+		fileParallelism: false,
+	},
 });
